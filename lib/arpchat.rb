33cee922809e72112d3981b665418217154b0567
Toshiyuki Shimanuki <happy.new@nyaa.info>
2013-12-13 23:36:17 +0900

    Procで定義してなくても落ちないようにした

--- lib/arpchat.rb
+++ lib/arpchat.rb
@@ -40,12 +40,17 @@ module ArpChat
     end
   end
 
-  class Proc
+  class Callback
     attr_accessor :receiveMessage, :leftPeer, :joinPeer
+    def initialize
+      @receiveMessage = Proc.new {|src, name, body| "#{name}(#{src}) #{body}" }
+      @leftPeer = Proc.new {|peer| "#{peer.ip} has left." }
+      @joinPeer = Proc.new {|src| "#{src} has joined." }
+    end
   end
 
   def configure(&block)
-    @@proc = Proc.new
+    @@proc = Callback.new
     block.call(@@proc)
   end
 
